a8c0cd60dc0936bba95b950bcba74ac1826dcaf7
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2024-07-15 14:59:22 +0800

    fixed broken JS

--- main.js
+++ main.js
@@ -1,50 +1,45 @@
 const Gameboard = (() => {
-    let gameboard = ["", "","","","","","","",""]
+    let gameboard = ["", "","","","","","","",""];
 
     const render = () => {
         let boardHTML = "";
         gameboard.forEach((square, index) => {
-            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
-        })
-    }
-    document.querySelector("gameboard").innerHTML = boardHTML;
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+        });
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+    };
     return {
         render,
-    }
+    };
 })();
 
 const createPlayer = (name, mark) => {
     return {
         name,
-        mark
-    }
-}
+        mark,
+    };
+};
 
 const Game = (() => {
-    let players = {};
+    let players = [];
     let currentPlayerIndex = 0;
-    let gameOver;
+    let gameOver = false;
 
     const start = () => {
         players = [
-            createPlayer(document.querySelector("player1").value, "X"),
-            createPlayer(document.querySelector("player2").value, "O"),
-        ]
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O"),
+        ];
         currentPlayerIndex = 0;
-        gameOver = false
-
-    }
-
+        gameOver = false;
+        Gameboard.render();
+    };
+    return {
+        start,
+    };
 })();
-
-
-
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
-    // 
     Game.start();
-
-
-
-})
+});
